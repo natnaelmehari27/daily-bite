3091c34920e2dba5ed5514102a00ed8542709db1
Natnael Mehari <natnaelmehari27@gmail.com>
2025-02-22 04:36:52 +0100

    create a comment form

--- news/forms.py
+++ news/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import NewsPost
+from .models import NewsPost, comment
 
 class NewsPostForm(forms.ModelForm):
     class Meta:
         model = NewsPost
         fields = ['title', 'content', 'category']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
