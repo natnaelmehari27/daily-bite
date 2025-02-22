3d100e44125c7d4988c7b0e958b89fadd8c719b3
Natnael Mehari <natnaelmehari27@gmail.com>
2025-02-22 04:33:04 +0100

    set the templates and urls for the details view

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.news_list, name='news_list'),
     path('create/', views.create_news_post, name='create_news_post'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
-from .models import NewsPost 
-from .forms import NewsPostForm
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import NewsPost, comment
+from .forms import NewsPostForm, CommentForm 
 # Create your views here.
 
 def news_list(request):
@@ -16,3 +16,25 @@ def create_news_post(request):
     else:
         form = NewsPostForm()
     return render(request, 'news/create_news_post.html', {'form': form})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(NewsPost, id=post_id)
+    comments = post.comments.all().order_by('-date_posted')
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.post = post
+            new_comment.user = request.user
+            new_comment.save()
+            return redirect('post_detail', post_id=post.id)
+    else:
+        form = CommentForm()
+
+    context = {
+        'post': post,
+        'comments': comments,
+        'form': form,
+    }
+    return render(request, 'news/post_detail.html', context)
